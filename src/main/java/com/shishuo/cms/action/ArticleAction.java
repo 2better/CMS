210c1a14e207a78c353da6d64b3da8588308eb3d
LanJianXi <735937769@qq.com>
2017-06-20 22:18:19 +0800

    整合搜索页面

--- src/main/java/com/shishuo/cms/action/ArticleAction.java
+++ src/main/java/com/shishuo/cms/action/ArticleAction.java
@@ -20,87 +20,92 @@ import java.util.Map;
 
 /**
  * @author Herbert
- * 
  */
 @Controller
 @RequestMapping("/article")
 public class ArticleAction extends BaseAction {
 
-	@Autowired
-	private MenuService menuService;
-	@Autowired
-	private ConfigService configService;
+    @Autowired
+    private MenuService menuService;
+    @Autowired
+    private ConfigService configService;
 
-	@RequestMapping(value = "/{articleId}.htm", method = RequestMethod.GET)
-	public String article(@PathVariable long articleId,
-						  ModelMap modelMap, HttpServletRequest request) {
-		try {
-			pageStaticUtils.articleStaticPage(request,articleId);
-			request.setAttribute("htmlName",articleId);
-			return "/template/blog/article_template";
-		} catch (Exception e) {
-			e.printStackTrace();
-			return themeService.get404();
-		}
-	}
+    @RequestMapping(value = "/{articleId}.htm", method = RequestMethod.GET)
+    public String article(@PathVariable long articleId,
+                          ModelMap modelMap, HttpServletRequest request) {
+        try {
+            pageStaticUtils.articleStaticPage(request, articleId);
+            request.setAttribute("htmlName", articleId);
+            return "/template/blog/article_template";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return themeService.get404();
+        }
+    }
 
-	@RequestMapping(value = "/list.htm", method = RequestMethod.GET)
-	public String list(@RequestParam(value = "menuId") long menuId,ModelMap modelMap,HttpServletRequest request) {
-		try {
+    @RequestMapping(value = "/list.htm", method = RequestMethod.GET)
+    public String list(@RequestParam(value = "menuId") long menuId, ModelMap modelMap, HttpServletRequest request) {
+        try {
 
-			Menu menu = menuService.getByid(menuId);
-			List<Menu> menus = menuService.getWithChildById(menu.getPid());
-			int count = fileService.getArticleCountByMenuId(menuId,"display");
-			int rows = configService.getIntKey("pagination_num");
-			int pageCount = fileService.getPageCount(count,rows);
-
-
-			modelMap.put("Menu",menu);
-			modelMap.put("menus",menus.get(0));
-			modelMap.put("count",count);
-			modelMap.put("pageCount",pageCount);
-
-			pageStaticUtils.headerAndFooterStaticPage(request);
-			return "template/blog/article_list";
-		} catch (Exception e) {
-			e.printStackTrace();
-			return themeService.get404();
-		}
-	}
-
-	@RequestMapping(value = "/listJson.json", method = RequestMethod.POST)
-	@ResponseBody
-	public Map<String,Object> listJson(@RequestParam(value = "menuId") long menuId,
-								 @RequestParam(value = "pageNum", defaultValue = "1") int pageNum) {
-		Map<String,Object> map = new HashMap<String,Object>();
-		int rows = configService.getIntKey("pagination_num");
-		map.put("list",fileService.getArticleByMenuId(menuId,pageNum,rows));
-		map.put("pageNum",pageNum);
-		return map;
-	}
-
-	@RequestMapping(value = "listNews", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Article> listNewsJson() {
-		return listArtice("149715108805446");
-	}
+            Menu menu = menuService.getByid(menuId);
+            List<Menu> menus = menuService.getWithChildById(menu.getPid());
+            int count = fileService.getArticleCountByMenuId(menuId, "display");
+            int rows = configService.getIntKey("pagination_num");
+            int pageCount = fileService.getPageCount(count, rows);
 
 
-	@RequestMapping(value = "listCooperation", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Article> listCooperationJson() {
-		return listArtice("149715115964151");
-	}
+            modelMap.put("Menu", menu);
+            modelMap.put("menus", menus.get(0));
+            modelMap.put("count", count);
+            modelMap.put("pageCount", pageCount);
 
-	private List<Article> listArtice(String menuId) {
-		return fileService.getArticeByMenuIdByTime(new Long(menuId),6);
-	}
+            pageStaticUtils.headerAndFooterStaticPage(request);
+            return "template/blog/article_list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return themeService.get404();
+        }
+    }
+
+    @RequestMapping(value = "/listJson.json", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> listJson(@RequestParam(value = "menuId") long menuId,
+                                        @RequestParam(value = "pageNum", defaultValue = "1") int pageNum) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        int rows = configService.getIntKey("pagination_num");
+        map.put("list", fileService.getArticleByMenuId(menuId, pageNum, rows));
+        map.put("pageNum", pageNum);
+        return map;
+    }
+
+    @RequestMapping(value = "listNews", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Article> listNewsJson() {
+        return listArtice("149715108805446");
+    }
 
 
-	@RequestMapping(value = "/search.json",method = RequestMethod.GET)
-	@ResponseBody
-	public PageVo<Article> getAticlesByKey(@RequestParam(value = "key",defaultValue = "") String key,
-										   @RequestParam(value = "p",defaultValue = "1") Integer p) {
-		return fileService.getArticlesBykey(configService.getIntKey("pagination_num"),p,key);
-	}
+    @RequestMapping(value = "listCooperation", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Article> listCooperationJson() {
+        return listArtice("149715115964151");
+    }
+
+    private List<Article> listArtice(String menuId) {
+        return fileService.getArticeByMenuIdByTime(new Long(menuId), 6);
+    }
+
+
+    @RequestMapping(value = "/search.json", method = RequestMethod.GET)
+    @ResponseBody
+    public PageVo<Article> getAticlesByKey(@RequestParam(value = "key", defaultValue = "") String key,
+                                           @RequestParam(value = "p", defaultValue = "1") Integer p) {
+        return fileService.getArticlesBykey(configService.getIntKey("pagination_num"), p, key);
+    }
+
+    @RequestMapping(value = "/search.html", method = RequestMethod.GET)
+    public String brower(@RequestParam(value = "key", defaultValue = "") String key,ModelMap m) {
+        m.put("key",key);
+        return "/template/blog/browser";
+    }
 }
